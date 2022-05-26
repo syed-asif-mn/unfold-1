299d558e231fc9a29c00d32dca799521f455586f
syed-asif-mn <syedasifm.n@gmail.com>
2022-05-26 21:59:58 +0530

    4 files modified, 2 added

--- src/app/View/view.component.ts
+++ src/app/View/view.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-view',
@@ -9,10 +10,18 @@ import { FormBuilder, Validators } from '@angular/forms';
 export class ViewComponent {
   serverFrameworks = ['C#', 'Python', 'Java', 'NodeJS', 'DJango'];
   clientFrameworks = ['Angular', 'React', 'JS'];
+  languages = ['English', 'हिंदी', 'Français', 'Deutsch', '中文', '日本語']
 
   isSubmitted = false;
 
-  constructor(public fb: FormBuilder) {}
+  constructor(public fb: FormBuilder, private translateService: TranslateService) {
+    this.translateService.use('en');
+    }
+    
+    changeLanguage(index: number) {
+    let codes = ['en', 'hin', 'fr', 'de', 'zh', 'ja']
+    this.translateService.use(codes[index]);
+    }
 
   /*########### Form ###########*/
   registrationForm = this.fb.group({
@@ -22,7 +31,7 @@ export class ViewComponent {
 
   // Getter method to access form control
   get myForm() {
-    return this.registrationForm.get('clientFramework');
+    return this.registrationForm.get('serverFramework') || this.registrationForm.get('clientFramework');
   }
 
   // Submit Registration Form
